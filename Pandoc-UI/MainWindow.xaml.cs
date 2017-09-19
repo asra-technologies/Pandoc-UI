f432bcc6e8035ada591320882a65f3a7d2657012
the-prism <t.castonguay.gagnon@gmail.com>
2017-09-19 09:50:29 -0400

    file prickers

--- Pandoc-UI/MainWindow.xaml.cs
+++ Pandoc-UI/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Pandoc_UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string OutputExtension;
+        private string OutputFormat;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,12 +29,69 @@ namespace Pandoc_UI
 
         private void SetInputFile(object sender, RoutedEventArgs e)
         {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = "";
+            dlg.Filter = "";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                InputFile.Text = filename;
+            }
         }
 
         private void SetOutputFile(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
 
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = OutputExtension;
+            dlg.Filter = OutputFormat;
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                OutputFile.Text = filename;
+            }
+        }
+
+        private void SaveFormatChange(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox fileFormat = sender as ComboBox;
+            ComboBoxItem selection = fileFormat.SelectedValue as ComboBoxItem;
+            
+            switch (selection.Content.ToString())
+            {
+                case "Pdf":
+                    OutputExtension = ".pdf";
+                    OutputFormat = "PDF (*.pdf)|*.pdf";
+                    break;
+                case "Word":
+                    OutputExtension = ".docx";
+                    OutputFormat = "Word Document (*.docx)|*.docx";
+                    break;
+                case "Html":
+                    OutputExtension = ".html";
+                    OutputFormat = "Html file (*.html)|*.html";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
