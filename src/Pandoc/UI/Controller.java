c329aaf5663212e1534aa6d76cf026335992f9b6
the-prism <t.castonguay.gagnon@gmail.com>
2017-09-25 14:04:51 -0400

    Set the file chooser

--- src/Pandoc/UI/Controller.java
+++ src/Pandoc/UI/Controller.java
@@ -3,8 +3,11 @@ package Pandoc.UI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
+
 import java.io.File;
+
 import javafx.stage.FileChooser;
+
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,7 +29,10 @@ public class Controller {
 
     @FXML
     protected void setOutputFile(ActionEvent event) {
-
+        File file = fileChooser.showSaveDialog(Main.stage);
+        if (file != null) {
+            openFile(file);
+        }
     }
 
     private void openFile(File file) {
@@ -38,5 +44,15 @@ public class Controller {
                     Level.SEVERE, null, ex
             );
         }
+    }
+
+    private static void configureFileChooser(final FileChooser fileChooser) {
+        fileChooser.setTitle("View Pictures");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
     }
 }
